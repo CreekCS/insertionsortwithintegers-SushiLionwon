d0c7f1d2aa05082034415a53da12235de2564631
SushiLionwon <ethanzhang1231@gmail.com>
2024-08-29 09:54:52 -0600

    Create InsSortInts.java

---
+++ InsSortInts.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class InsSortInts {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(new File("numbers.txt"));
+        int[] arr = new int[1000]; 
+        int count = 0;
+
+        while (scanner.hasNextInt()) {
+            arr[count++] = scanner.nextInt();
+        }
+        scanner.close();
+
+        insertionSort(arr, count);
+
+        System.out.println("Smallest value: " + arr[0]);
+        System.out.println("Largest value: " + arr[count - 1]);
+    }
+
+    public static void insertionSort(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+            int key = arr[i], j = i - 1;
+            while (j >= 0 && arr[j] > key) arr[j + 1] = arr[j--];
+            arr[j + 1] = key;
+        }
+    }
+}
